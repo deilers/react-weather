35885b0745dd4b8d9346b898b86e9b9a1097fc28
Devon Eilers <devon.d.eilers@boeing.com>
2018-01-13 16:52:56 -0800

    intial commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '858c0128af15a49086a757cef247ce09';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
